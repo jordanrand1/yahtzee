c0bf8c2cc70259562a23d8a11fe8fe305c407386
Jordan Rand <jorrand@gmail.com>
2018-10-16 11:26:23 -0600

    added rollable dice

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -17,17 +17,39 @@ const ScoreContainer = styled(FullHeight)`
   background-color: #9370DB;
 `
 
-const Game = () => (
-  <Grid>
-    <Grid.Row>
-      <BoardContainer width={10}>
-        <Board />
-      </BoardContainer>
-      <ScoreContainer width={6}>
-        <ScoreCard />
-      </ScoreContainer>
-    </Grid.Row>
-  </Grid>
-)
+class Game extends React.Component {
+  state = { roll: 0, keep: [], dice: [...new Array(5)] }
+
+  rollDice = () => {
+    const dice = this.state.dice.map( (el, i) => {
+      return Math.floor(Math.random() * 6) + 1
+    })
+
+    this.setState( state => { 
+      return { dice, roll: state.roll + 1 }
+    })
+  }
+
+  render () {
+    const { roll, dice } = this.state
+
+    return (
+      <Grid>
+        <Grid.Row>
+          <BoardContainer width={10}>
+            <Board 
+              roll={roll}
+              dice={dice}
+              rollDice={this.rollDice}
+            />
+          </BoardContainer>
+          <ScoreContainer width={6}>
+            <ScoreCard />
+          </ScoreContainer>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+}
 
 export default Game
