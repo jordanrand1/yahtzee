b5c4717df297b966237e997afd27f4e89f04c9e0
Jordan Rand <jorrand@gmail.com>
2018-10-16 10:45:58 -0600

    ?

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,1 +1,33 @@
-export default () => null
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import Board from './Board'
+import ScoreCard from './ScoreCard'
+import styled from 'styled-components'
+
+const FullHeight = styled(Grid.Column)`
+  height: 100vh;
+
+`
+
+const BoardContainer = styled(FullHeight)`
+  background-color: #AAFFAA;
+`
+
+const ScoreContainer = styled(FullHeight)`
+  background-color: #9370DB;
+`
+
+const Game = () => (
+  <Grid>
+    <Grid.Row>
+      <BoardContainer width={10}>
+        <Board />
+      </BoardContainer>
+      <ScoreContainer width={6}>
+        <Score />
+      </ScoreContainer>
+    </Grid.Row>
+  </Grid>
+)
+
+export default Game
